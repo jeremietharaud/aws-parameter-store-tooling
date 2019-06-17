cc7d50223dbf64c7ced436b230d68783d725d21f
Jeremie Tharaud <jeremie.tharaud@d2si.io>
2019-06-17 11:14:54 +0200

    add new options: delete and import using kms

--- README.md
+++ README.md
@@ -13,3 +13,14 @@ To retrieve all parameters:
 ```python manage.py -e '//'```
 
 Be careful of the interpretation of the slash. You may have to escape them using double slash and bashslash.
+
+Example for importing parameters from a local file (in key/value format):
+```python manage.py -i 'file'```
+
+For encrypting parameters using a KMS key (AWS managed key or CMK), use the `-k` option:
+```python manage.py -i 'file' -k alias/aws/ssm```
+
+```python manage.py -i 'file' -k <kmskeyid>```
+
+For deleting parameters from a local file (in key/value format):
+```python manage.py -d 'file'```
--- manage.py
+++ manage.py
@@ -30,6 +30,16 @@ class ParameterStore:
             parameters = np.concatenate((parameters, response['Parameters']))
         return parameters
 
+    def delete_parameters(self, file):
+        f = open(file, "r")
+        kv = json.load(f)
+        response = self.client.delete_parameters(
+            Names=list(kv.keys())
+        )
+        print("The following parameters have been deleted: ",
+              response['DeletedParameters'])
+        return
+
     def get_parameter_value(self, param):
         response = self.client.get_parameter(
             Name=param,
@@ -37,38 +47,66 @@ class ParameterStore:
         )
         return response['Parameter']['Value']
 
-    def put_parameter(self, key, value):
+    def put_parameter(self, key, value, kms_key):
         try:
             current_value = self.get_parameter_value(key)
         except botocore.exceptions.ClientError as e:
             if e.response.get("Error").get("Code") == "ParameterNotFound":
-                self.client.put_parameter(
-                    Name=key,
-                    Type='String',
-                    Value=value
-                )
+                if kms_key is None:
+                    self.client.put_parameter(
+                        Name=key,
+                        Type='String',
+                        Value=value
+                    )
+                elif kms_key == "default":
+                    self.client.put_parameter(
+                        Name=key,
+                        Type='SecureString',
+                        Value=value
+                    )
+                else:
+                    self.client.put_parameter(
+                        Name=key,
+                        Type='SecureString',
+                        KeyId=kms_key,
+                        Value=value
+                    )
                 print("Parameter %s has been added" % key)
                 exit(0)
             else:
                 raise e
-        if value == current_value:
+        if value == current_value and kms_key is None:
             print("No update for", key)
         else:
-            self.client.put_parameter(
-                Name=key,
-                Overwrite=True,
-                Type='String',
-                Value=value
-            )
+            if kms_key is None:
+                self.client.put_parameter(
+                    Name=key,
+                    Overwrite=True,
+                    Type='String',
+                    Value=value
+                )
+            elif kms_key == "default":
+                self.client.put_parameter(
+                    Name=key,
+                    Overwrite=True,
+                    Type='SecureString',
+                    Value=value
+                )
+            else:
+                self.client.put_parameter(
+                    Name=key,
+                    Overwrite=True,
+                    Type='SecureString',
+                    KeyId=kms_key,
+                    Value=value
+                )
             print("Parameter %s has been updated" % key)
 
-
-    def put_parameters(self, file):
+    def put_parameters(self, file, kms_key):
         f = open(file, "r")
         kv = json.load(f)
         for key, value in kv.items():
-            self.put_parameter(key, value)
-
+            self.put_parameter(key, value, kms_key)
 
     def get_parameters_by_path(self, path='/'):
         response = self.client.get_parameters_by_path(
@@ -97,25 +135,51 @@ class ParameterStore:
         return json.dumps(kv, indent=4, sort_keys=True)
 
 
+def usage():
+    print('Usage: manage.py --export <parameterpath> | --import <file> | --key <kmskeyid> | --delete')
+
+
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv, "he:i:", ["help","export=", "import="])
+        opts, args = getopt.getopt(
+            argv, "he:i:k:d:",
+            ["help", "export=", "import=", 'key=', "delete="])
     except getopt.GetoptError:
-        print('Usage: manage.py export <parameterpath> | import <file>')
+        usage()
         sys.exit(2)
+    action = None
+    action_arg = None
+    key = None
     for opt, arg in opts:
         if opt in ("help", "-h"):
-            print('Usage: manage.py export <parameterpath> | import <file>')
+            usage()
             sys.exit()
-        elif opt in ("export", "-e"):
-            store = ParameterStore()
-            print(store.get_parameters_by_path(arg))
+        elif opt in ("--export", "-e"):
+            action = "export"
+            action_arg = arg
+        elif opt in ("--import", "-i"):
+            action = "import"
+            action_arg = arg
+        elif opt in ("--key", "-k"):
+            key = arg
+        elif opt in ("--delete", "-d"):
+            action = "delete"
+            action_arg = arg
+        else:
+            usage()
             sys.exit()
-        elif opt in ("import", "-i"):
-            store = ParameterStore()
-            store.put_parameters(arg)
-            sys.exit()
-    print('Usage: manage.py export <parameterpath> | import <file>')
+
+    if action == "export":
+        store = ParameterStore()
+        print(store.get_parameters_by_path(action_arg))
+    elif action == "import":
+        store = ParameterStore()
+        store.put_parameters(action_arg, key)
+    elif action == "delete":
+        store = ParameterStore()
+        store.delete_parameters(action_arg)
+    else:
+        usage()
 
 
 if __name__ == "__main__":
